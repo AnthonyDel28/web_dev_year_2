d32725616c8ebfd28f294106fcb5ad3a11d1bf19
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-09-23 14:00:02 +0200

    update 23_09_2022

--- POO_Framework/Projet/app/Articles.php
+++ POO_Framework/Projet/app/Articles.php
@@ -13,12 +13,16 @@ class Articles extends DB
         return $result = $stmt->fetchAll(); // tableau d'objets mis en fetch_obj par default dans notre class DB
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function getArticle ($id)
     {
-
-        $query= 'SELECT * FROM post WHERE id = ?';
+        $query= 'SELECT * FROM post WHERE id = :id';
         $stmt = $this->dbh->prepare($query);
-        $stmt->execute([$id]);
-        return $article = $stmt->fetch();
+        $stmt->bindValue('id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+        return $result = $stmt->fetch();
     }
 }

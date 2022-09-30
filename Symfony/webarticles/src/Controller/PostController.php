903abf50b65ed9a2efbce35cd67385983fc860ee
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-09-30 14:53:37 +0200

    update 30_09_2022

--- Symfony/webarticles/src/Controller/PostController.php
+++ Symfony/webarticles/src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/', name: 'posts')]
-    public function posts(): Response
+    public function posts(PostRepository $repository): Response
     {
+        $posts = $repository->findBy(array('isPublished' => true), array('title' => 'ASC'));
         return $this->render('post/posts.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
         ]);
     }
 }

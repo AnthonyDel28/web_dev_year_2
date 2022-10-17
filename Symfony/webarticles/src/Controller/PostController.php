bb9672821ad2b49cba2a396be08679338bc310e8
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-10-17 23:21:43 +0200

    update 17_10_2022

--- Symfony/webarticles/src/Controller/PostController.php
+++ Symfony/webarticles/src/Controller/PostController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
@@ -15,11 +17,17 @@ class PostController extends AbstractController
    * @return Response
    */
   #[Route('/', name: 'posts')]
-  public function Posts(PostRepository $repository): Response
+  public function Posts(PostRepository $repository, PaginatorInterface $paginator, Request $request): Response
   {
     $posts = $repository->findBy(
         ['isPublished' => true],
         ['title' => 'ASC']
+      );
+
+      $posts = $paginator->paginate(
+          $posts,
+          $request->query->getInt('page', 1),
+          10
       );
     return $this->render('post/posts.html.twig', ['posts' => $posts]);
   }

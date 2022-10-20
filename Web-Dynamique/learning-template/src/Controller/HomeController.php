a2456bea9c3660431d309f3831364cdccea0e9fc
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-10-20 21:34:05 +0200

    update 20_10_2022

--- Web-Dynamique/learning-template/src/Controller/HomeController.php
+++ Web-Dynamique/learning-template/src/Controller/HomeController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Course;
+use App\Controller\CourseController;
+use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,8 +14,22 @@ class HomeController extends AbstractController
    * @return Response
    */
   #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(CourseRepository $courseRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $courses = $courseRepository->findBy(['is_published' => true], ['name' => 'DESC'], 3);
+        return $this->render('home/index.html.twig', [
+            'courses' => $courses
+        ]);
+    }
+
+    /**
+     * @return Response
+     */
+    #[Route('course/{slug}', name: 'course')]
+    public function course(Course $course): Response
+    {
+        return $this->render('course/course.html.twig', [
+            'course' => $course
+        ]);
     }
 }

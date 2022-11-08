a377d771f5a885ae31600c1fd1152177f5337701
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-11-08 14:36:35 +0100

    update 08_11_2022

--- Vue.js/14/14.js
+++ Vue.js/14/14.js
@@ -1,45 +1,47 @@
 const app = Vue.createApp({
     data() {
         return {
-          personnes: [
-              { prenom: "Adrien", sexe: "M", age: 34},
-              { prenom: "Mathilde", sexe: "F", age: 29},
-              { prenom: "Julien", sexe: "M", age: 26},
-              { prenom: "Anna", sexe: "F", age: 26},
-              { prenom: "Evan", sexe: "M", age: 25},
-              { prenom: "Eva", sexe: "F", age: 28},
-              { prenom: "Axel", sexe: "M", age: 28},
-          ],
-            names: [],
+            personnes: [
+                {prenom: "Adrien", sexe: "M", age: 34},
+                {prenom: "Mathilde", sexe: "F", age: 29},
+                {prenom: "Julien", sexe: "M", age: 26},
+                {prenom: "Anna", sexe: "F", age: 26},
+                {prenom: "Evan", sexe: "M", age: 25},
+                {prenom: "Eva", sexe: "F", age: 28},
+                {prenom: "Axel", sexe: "M", age: 28},
+            ],
+            persChoisies: null,
+            sexeChoisi: "X",
+            ageMin: 0,
+            ageMax: 200,
         }
     },
 
     methods: {
-        all() {
-            this.names = [];
-            for (person in this.personnes){
-                this.names.push(this.personnes[person].prenom);
-            }
-        },
 
-        male() {
-            this.names = [];
-            for (person in this.personnes){
-                if(this.personnes[person].sexe === 'M'){
-                    this.names.push(this.personnes[person].prenom);
-                }
-            }
-        },
-
-        female() {
-            this.names = [];
-            for (person in this.personnes){
-                if(this.personnes[person].sexe === 'F'){
-                    this.names.push(this.personnes[person].prenom);
-                }
-            }
-        }
     },
+
+    computed: {
+        personnesFiltrees(){
+            this.persChoisies = null;
+            let tabFiltre = this.personnes.filter(p => p.age >= this.ageMin && p.age <= this.ageMax)
+            switch(this.sexeChoisi){
+                case "X":
+                    return tabFiltre;
+                    break;
+                case "F":
+                    return tabFiltre.filter(p=>p.sexe == "F");
+                    break;
+                case "M":
+                    return tabFiltre.filter(p=>p.sexe == "M");
+                    break;
+            }
+        },
+        getPersonAge() {
+            if(this.persChoisies == null) return;
+            else return this.personnes.find(p => p.prenom == this.persChoisies).age;
+        }
+    }
 });
 
 app.mount("#app");

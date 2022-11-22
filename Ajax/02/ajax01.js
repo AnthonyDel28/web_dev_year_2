8bdd7161f5f26fb10c7f0b555acab9d7e8d6fe5d
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-11-22 15:11:57 +0100

    update 22_11_2022

--- Ajax/02/ajax01.js
+++ Ajax/02/ajax01.js
@@ -2,23 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       listePays: [],
-      listePaysSelectionnes: [],
-      codeChoisi: null, //alpha2P
+      paysChoisis: [],
+      paysChoisi: null,
     };
   },
   methods: {
-    ajouterPays(){
-      if(this.codeChoisi == null) return;
-      if(this.paysSelectionne(this.codeChoisi)) return;
-      this.listePaysSelectionnes.push(this.paysChoisi());
+    addCountry(){
+        this.paysChoisis.push(this.paysChoisi);
     },
-    paysSelectionne(code2P){
-      return this.listePaysSelectionnes.findIndex(p => p.alpha2P === code2P) >= 0;
-    }
+
   },
   computed: {
-    paysChoisi() {
-      return this.listePays.find(p => p.alpha2P === this.codeChoisi);
+    paysChoisi(){
+      return this.listePays.find(p => p.alpha2P == this.codeChoisi);
     },
     listePaysTries() {
       return [...this.listePays].sort((a, b) => a.nomFrP.localeCompare(b.nomFrP));
@@ -29,7 +25,6 @@ const app = Vue.createApp({
       .then(rep => rep.json()) // transformation de la réponse du serveur
       .then(liste => {
         // utilisation de la réponse transformée
-
         this.listePays = liste;
       });
   },

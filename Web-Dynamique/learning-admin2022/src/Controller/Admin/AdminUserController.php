b93b9cc6e96c4a51c3106f635d7ddabcb3adff7d
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-12-05 20:43:24 +0100

    update 05_12_2022

--- Web-Dynamique/learning-admin2022/src/Controller/Admin/AdminUserController.php
+++ Web-Dynamique/learning-admin2022/src/Controller/Admin/AdminUserController.php
@@ -1,18 +1,54 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminUserController extends AbstractController
 {
+    /**
+     * @param UserRepository $repository
+     * @return Response
+     */
     #[Route('/admin/user', name: 'app_admin_user')]
-    public function index(): Response
+    public function adminUsers(UserRepository $repository): Response
     {
-        return $this->render('admin_user/index.html.twig', [
-            'controller_name' => 'AdminUserController',
+        $users = $repository->findAll();
+        return $this->render('admin/users/users.html.twig', [
+            'users' => $users,
         ]);
+    }
+
+
+    #[Route('/admin/role_user/{id}', name: 'app_admin_role_user_user')]
+    public function userToUser(User $user, EntityManagerInterface $manager): Response
+    {
+        $role[] = 'ROLE_USER';
+       $user->setRoles($role);
+       $manager->flush();
+        return $this->redirectToRoute('app_admin_user');
+    }
+
+    #[Route('/admin/role_admin/{id}', name: 'app_admin_role_user_admin')]
+    public function userToAdmin(User $user, EntityManagerInterface $manager): Response
+    {
+        $role[] = 'ROLE_ADMIN';
+        $user->setRoles($role);
+        $manager->flush();
+        return $this->redirectToRoute('app_admin_user');
+    }
+
+    #[Route('/admin/role_super_admin/{id}', name: 'app_admin_role_user_super_admin')]
+    public function userToSuperAdmin(User $user, EntityManagerInterface $manager): Response
+    {
+        $role[] = 'ROLE_SUPER_ADMIN';
+        $user->setRoles($role);
+        $manager->flush();
+        return $this->redirectToRoute('app_admin_user');
     }
 }

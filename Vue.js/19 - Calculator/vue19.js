64f5d62cfb81be6d04103383436aba3aa1284f8a
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-12-10 17:03:37 +0100

    update 10_12_2022

--- Vue.js/19 - Calculator/vue19.js
+++ Vue.js/19 - Calculator/vue19.js
@@ -25,22 +25,44 @@ const app = Vue.createApp({
         status_checker() {
             switch (this.status){
                 case 'registration':
+                    this.pseudo = null;
+                    this.count = 0;
+                    this.score = 0;
+                    this.timing = 0;
+                    this.calcul = [
+                        [
+                            Math.floor(Math.random() * 101),
+                            Math.floor(Math.random() * 101),
+                            0,
+                            0,
+                        ],
+                    ];
+                    document.getElementById('register').style.display = "block";
                     document.getElementById('countdown').style.display = "none";
                     document.getElementById('exam').style.display = "none";
+                    document.getElementById('scoreboard').style.display = "none";
                     break;
                 case 'countdown':
                     document.getElementById('register').style.display = "none";
                     document.getElementById('exam').style.display = "none";
                     document.getElementById('countdown').style.display = "block";
                     this.createAlert();
-                    setTimeout(this.status = "running", 5000);
-                    setTimeout(this.status_checker, 6000);
+                    setTimeout(() => {
+                       this.status = "running";
+                       this.status_checker();
+                    }, 6000);
                     break;
                 case 'running':
                     document.getElementById('countdown').style.display = "none";
                     document.getElementById('exam').style.display = "block";
                     this.createExamCountdown();
-                    setTimeout(this.status = "finished", 10000);
+                    this.timing = 1;
+                    this.time();
+                    break;
+                case 'finished':
+                    document.getElementById('exam').style.display = "none";
+                    document.getElementById('scoreboard').style.display = "block";
+                    this.scoreboard();
                     break;
             }
         },
@@ -58,13 +80,11 @@ const app = Vue.createApp({
             this.calcul[this.count][2] = 1;
             if(((this.calcul[this.count][0]) + this.calcul[this.count][1]) == this.calcul[this.count][3]){
                 this.score++;
-                console.log(this.score);
             }
             this.count++;
             this.calcul[this.count] = [];
             this.calcul[this.count].push(Math.floor(Math.random() * 101), Math.floor(Math.random() * 101), this.count);
             this.calcul[this.count][2] = 0;
-            this.timing = 1;
         },
         createExamCountdown(){
             let timeLeft = 10;
@@ -82,6 +102,23 @@ const app = Vue.createApp({
                 }
             }, 1000);
         },
+        time(){
+            if(this.timing == 1){
+                setTimeout(() => {
+                    this.status = "finished";
+                    this.calcul[this.count][2] = 1;
+                    this.status_checker();
+                }, 11500);
+            }
+        },
+        scoreboard(){
+            document.getElementById("final-score").innerHTML = "Votre score: " + this.score;
+        },
+        restart(){
+            this.status = 'registration';
+            this.status_checker();
+        }
+
     },
     computed: {
 
@@ -93,32 +130,3 @@ const app = Vue.createApp({
 
 app.mount("#app");
 
-
-/*
-calculator(){
-            this.calcul[this.count][2] = 1;
-            if((this.calcul[this.count][0] + this.calcul[this.count][1]) == this.calcul[this.count][3]){
-                this.score++;
-            }
-            this.count++;
-            this.calcul[this.count] = [];
-            this.calcul[this.count].push(Math.floor(Math.random() * 101), Math.floor(Math.random() * 101), this.count);
-            this.calcul[this.count][2] = 0;
-            this.timing = 1;
-            this.time();
-            },
-
-        time(){
-            if(this.timing == 1)
-            {
-                setTimeout(() => {
-                    this.end();
-                }, 10000)
-            }
-        },
-
-        end(){
-            this.calcul[this.count][2] = 1;
-            document.getElementById("score").innerHTML = "Votre score: " + this.score;
-        }
- */

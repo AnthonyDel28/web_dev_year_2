2d164304f88255ed747b35144a50f3abd9c86249
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-12-20 07:40:31 +0100

    update 20_12_2022

--- Vue.js/19 - Calculator/save.php
+++ Vue.js/19 - Calculator/save.php
@@ -1,17 +1,15 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'mathscore');
+$pdo = new PDO(
+    'mysql:host=localhost; dbname=mathscore; charset=utf8',
+    'root',
+    ''
+);
 $pseudo = $_POST['pseudo'];
 $score = $_POST['score'];
 $date = new DateTimeImmutable();
-$sql = "INSERT INTO `scores`(`pseudo`, `score`, `date`) 
-	VALUES ('$pseudo','$score', now())";
+$request = $pdo->prepare("INSERT INTO `scores`(`pseudo`, `score`, `date`) 
+	VALUES ('$pseudo','$score', now())");
+$request->execute();
 
-    if ($conn->query($sql) === TRUE) {
-        echo "data inserted";
-    }
-    else
-    {
-        echo "failed";
-    }
 
 ?>
